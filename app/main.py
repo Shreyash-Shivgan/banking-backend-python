7408a1939532c6dbe62fbac0695d331469be7e05
Shreyash-Shivgan <shreyashivgan1234@gmail.com>
2026-02-02 21:21:27 +0530

    feat: add JWT authentication and protected user endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from app.users import users_db
 from app.security import hash_password, verify_password
+from app.security import create_access_token
+from fastapi import Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import JWTError, jwt
+from app.security import SECRET_KEY, ALGORITHM
 
 app = FastAPI(title="Banking Backend API")
+security = HTTPBearer()
 
 @app.get("/health")
 def health_check():
@@ -31,4 +37,41 @@ def login(email: str, password: str):
     if not verify_password(password, user["password"]):
         raise HTTPException(status_code=400, detail="Invalid credentials")
 
-    return {"message": "Login successful"}
+    access_token = create_access_token(
+    data={"sub": user["email"]}
+    )
+    return {
+    "access_token": access_token,
+    "token_type": "bearer"
+    }
+
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+):
+    token = credentials.credentials
+
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = users_db.get(email)
+    if user is None:
+        raise credentials_exception
+
+    return user
+
+@app.get("/me")
+def read_me(current_user: dict = Depends(get_current_user)):
+    return {
+        "email": current_user["email"],
+        "role": current_user["role"]
+    }
